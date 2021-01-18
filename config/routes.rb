c0eeaf0f35dd4da2ba63e78286c0bc53ef5eb43d
Victor Batista <v.rotciv@hotmail.com>
2021-01-18 06:52:36 -0300

    changing 'profile' name to 'user'

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  namespace :profiles_backoffice do
+  namespace :site do
+    get 'welcome/index'
+  end
+  namespace :users_backoffice do
     get 'welcome/index'
   end
   namespace :admins_backoffice do
     get 'welcome/index'
   end
-  devise_for :profiles
   devise_for :admins
-  root to: 'welcome#index'
+  devise_for :users
+  root to: 'site/welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
